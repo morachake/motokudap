e343398a1887c6171051457576b6da1d82a3fbeb
jacobmoracha@gmail.com <jacobmoracha@gmail.com>
2023-10-09 23:39:31 +0300

    connecting frontend

--- src/dbank_frontend/src/index.js
+++ src/dbank_frontend/src/index.js
@@ -2,15 +2,41 @@ import {dbank_backend} from "../../declarations/dbank_backend"
 
 window.addEventListener("load", async function () {
   console.log("loading")
-  const currentAmount = await dbank_backend.checkBalance();
-  this.document.getElementById("value").innerHTML = Math.round(currentAmount * 100 )
+ update()
 })
 
 document.querySelector("form").addEventListener("submit",async function (event){
   event.preventDefault();
   console.log("submitted")
-  const inputAmount =  document.getElementById("input-amount").value;
-  const outputAmount = document.getElementById("withdrawal-amount").value;
 
-  await dbank_backend.topUp(inputAmount)
+  const button = event.target.querySelector("#submit-btn");
+
+  const inputAmount =  parseFloat(document.getElementById("input-amount").value);
+
+  const outputAmount = parseFloat(document.getElementById("withdrawal-amount").value);
+
+  button.setAttribute("disabled", true)
+
+  if (document.getElementById("input-amount").value.length != 0){
+    await dbank_backend.topUp(inputAmount);
+  }
+  
+  if (document.getElementById("withdrawal-amount").value.length != 0){
+    await dbank_backend.withDraw(outputAmount)
+  }
+
+  await dbank_backend.compound();
+
+  update()
+
+  document.getElementById("input-amount").value = "";
+  document.getElementById("withdrawal-amount").value = "";
+
+  button.removeAttribute("disabled")
+
 })
+
+async function update (){
+  const currentAmount = await dbank_backend.checkBalance();
+  document.getElementById("value").innerHTML = Math.round(currentAmount * 100 )
+}
